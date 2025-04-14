e1541ceb763794b5c25a42340523b403a49a5772
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-04-14 23:22:04 +0100

    feat: add immutable vars

---
+++ src/StakingReward.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.29;
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract StakingReward {
+    IERC20 public immutable stakedToken;
+    IERC20 public immutable rewardToken;
+    uint256 public immutable rewardRatePerSecond;
+    uint256 public immutable rewardsEndTime;
+
+    constructor(address _stakedToken, address _rewardToken, uint256 _totalReward, uint256 _rewardDuration) {
+        stakedToken = IERC20(_stakedToken);
+        rewardToken = IERC20(_rewardToken);
+        rewardRatePerSecond = _totalReward / _rewardDuration;
+        rewardsEndTime = block.timestamp + _rewardDuration;
+    }
+}
