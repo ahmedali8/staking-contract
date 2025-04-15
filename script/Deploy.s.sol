528625c89f840fb771372042ecae8541edadc3a7
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-04-15 22:48:06 +0100

    feat: add a deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.8.29 <0.9.0;
 
-import { Foo } from "../src/Foo.sol";
-
 import { BaseScript } from "./Base.s.sol";
+
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import { StakingReward } from "../src/StakingReward.sol";
+import { TokenR } from "../src/TokenR.sol";
+import { TokenT } from "../src/TokenT.sol";
 
 /// @dev See the Solidity Scripting tutorial: https://book.getfoundry.sh/guides/scripting-with-solidity
 contract Deploy is BaseScript {
-    function run() public broadcast returns (Foo foo) {
-        foo = new Foo();
+    function run(
+        uint256 totalReward,
+        uint256 rewardDuration
+    )
+        public
+        broadcast
+        returns (TokenT tokenT, TokenR tokenR, StakingReward stakingReward)
+    {
+        tokenT = deployTokenT();
+        tokenR = deployTokenR();
+        stakingReward = deployStakingReward(tokenT, tokenR, totalReward, rewardDuration);
+    }
+
+    function deployTokenT() public returns (TokenT tokenT) {
+        tokenT = new TokenT();
+        vm.label(address(tokenT), "TokenT");
+    }
+
+    function deployTokenR() public returns (TokenR tokenR) {
+        tokenR = new TokenR();
+        vm.label(address(tokenR), "TokenR");
+    }
+
+    function deployStakingReward(
+        IERC20 stakedToken,
+        IERC20 rewardToken,
+        uint256 totalReward,
+        uint256 rewardDuration
+    )
+        public
+        returns (StakingReward stakingReward)
+    {
+        stakingReward = new StakingReward(stakedToken, rewardToken, totalReward, rewardDuration);
+        vm.label(address(stakingReward), "StakingReward");
     }
 }
