eea0f6bf81515ea9d8f101ccf1b43359344b14a0
Ahmed Ali <imahmedalibhatti@gmail.com>
2025-04-16 22:14:54 +0100

    test: add constants

--- tests/Base.t.sol
+++ tests/Base.t.sol
@@ -19,7 +19,9 @@ abstract contract Base_Test is Test {
     uint40 internal constant APRIL_1_2025 = 1_743_462_000;
     uint256 internal constant TOTAL_REWARD = 1_000_000 ether;
     uint256 internal constant REWARD_DURATION = 365 days;
-    uint256 internal constant PRECISION = 1e18;
+    uint256 internal constant WAD = 1e18;
+    uint256 internal constant ONE_MILLION_TOKENS = 1_000_000 ether;
+    uint256 internal constant ONE_SECOND = 1;
 
     Users internal users;
 
